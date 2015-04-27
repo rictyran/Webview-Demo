350c410ea343e82914670dc1d4fbba9acf62ae1c
rictyran <rictyran@gmail.com>
2015-04-27 14:45:45 -0400

    Show web content in an iOS app built in Swift
    final

---
+++ Webview Demo/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Webview Demo
+//
+//  Created by Richard Tyran on 4/26/15.
+//  Copyright (c) 2015 Richard Tyran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var webview: UIWebView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+//    
+//        var url = NSURL(string: "http://www.google.com")
+//        
+//        var request = NSURLRequest(URL:url!)
+//    
+//        webview.loadRequest(request)
+        
+        var html = "<html><head></head><body><h1>Hello World!</h1></body></html>"
+        
+        webview.loadHTMLString(html, baseURL: nil)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
